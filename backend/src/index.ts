a4d6f38657fddb730a74847a7e91d86f2ed69736
Sameer Marathe <sameer.marathe@datenwissen.com>
2024-11-02 11:48:32 +0530

    One way Communication established , frontend init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import {WebSocketServer}  from 'ws';
+import {WebSocketServer, WebSocket}  from 'ws';
 
 const wss = new WebSocketServer({port: 8080});
 
@@ -7,8 +7,28 @@ let receiverSocket : null | WebSocket  = null;
 
 wss.on('connection',(ws)=>{
     ws.on('message',(msg:any)=>{
-        const newMessage  =  JSON.parse(msg);
-        console.log(`Received message => ${newMessage.name}`);
+        const message  =  JSON.parse(msg);
+        console.log(`Received message => ${message.name}`);
+
+        if(message.type === 'identify-as-sender'){
+            senderSocket = ws;
+        }
+        else if(message.type === 'identify-as-receiver'){
+            receiverSocket = ws;
+        }
+        else if(message.type === 'create-offer'){
+            receiverSocket?.send(JSON.stringify({type:'offer',sdp:message.offer}));
+        }
+        else if(message.type === 'create-answer'){
+            senderSocket?.send(JSON.stringify({type:'answer',sdp:message.answer}));
+        }
+        else if(message.type === 'ice-candidate'){
+            if(ws ===senderSocket){
+                receiverSocket?.send((JSON.stringify({type:'ice-candidate',candidate:message.candidate})));
+            }
+            else if(ws === receiverSocket){
+                senderSocket?.send((JSON.stringify({type:'ice-candidate',candidate:message.candidate})));
+            }
+        }
     })
 })
-//video left at 20:00
---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Sender from './components/Sender'
+import Receiver from './components/Receiver'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/sender' element={<Sender/>}/>
+        <Route path='/receiver' element={<Receiver/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
